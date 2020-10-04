69ea709a74866a5e5eb7531634263e4aa37e476b
Sam Kim <samuyyy@gmail.com>
2020-10-04 18:50:47 -0400

    fixed comment to show accurate example of example url for businessController.getBusinesses

--- server/routes/businesses.js
+++ server/routes/businesses.js
@@ -3,10 +3,14 @@ const businessesController = require('../controllers/businessesController.js');
 
 const router = express.Router();
 
-router.get('/:category', businessesController.getBusinessData, (req, res) => {
+router.get('/:category', businessesController.getBusinesses, (req, res) => {
   /* include lat and lon in query string
+   *
    * example url where 'restaurants' is the category:
-   * http://localhost:3000/restaurants?lat=40.712775&lon=-74.005973
+   * http://localhost:5000/businesses/restaurants?lat=40.712775&lon=-74.005973
+   *
+   * server response from the url above should be an array of 5 objects.
+   * each object contains information on a restaurant
    */
 
   const { businesses } = res.locals;
