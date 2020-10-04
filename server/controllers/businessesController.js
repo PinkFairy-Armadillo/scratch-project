c93b94de96233465ea704eecf4688d29d2e9821a
Sam Kim <samuyyy@gmail.com>
2020-10-04 19:16:27 -0400

    implement route handler to get businesses by price

--- server/controllers/businessesController.js
+++ server/controllers/businessesController.js
@@ -9,6 +9,7 @@ businessesController.getBusinesses = (req, res, next) => {
   const { category } = req.params;
   const { lat, lon } = req.query;
 
+  // log error if latitude or longitude are undefined
   if (lat === undefined || lon === undefined) {
     return next({
       log: 'businessController.getBusinesses: ERROR: lat and/or lon are undefined',
@@ -32,8 +33,42 @@ businessesController.getBusinesses = (req, res, next) => {
     .catch((err) => next(err));
 };
 
-// TODO: create controller to handle getting top 5 businesses by price
 businessesController.getBusinessesByPrice = (req, res, next) => {
+  console.log('entering getBusinessesByPrice');
+  const { category, priceLevel } = req.params;
+  const { lat, lon } = req.query;
+
+  // log error if latitude or longitude are undefined
+  if (lat === undefined || lon === undefined) {
+    return next({
+      log: 'businessController.getBusinesses: ERROR: lat and/or lon are undefined',
+      message: { err: 'businessesController.getBusinesses: ERROR: Check server logs for details' },
+    });
+  }
+
+  // log error if priceLevel is an invalid input
+  if (priceLevel < 1 || priceLevel > 4) {
+    return next({
+      log: 'businessesController.getBusinessesByPrice: ERROR: priceLevel is invalid. priceLevel must be: 1 <= priceLevel <= 4.',
+      message: { err: 'businessesController.getBusinessesByPrice: ERROR: Check server logs for details' },
+    });
+  }
+
+  const limit = 5; // we can make this dynamic later
+  const url = `https://api.yelp.com/v3/businesses/search?latitude=${lat}&longitude=${lon}&categories=${category}&price=${priceLevel}&limit=${limit}`;
+
+  fetch(url, {
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+    },
+  })
+    .then((data) => (data.json()))
+    .then(({ businesses }) => {
+      console.log(businesses);
+      res.locals.businesses = businesses;
+      return next();
+    })
+    .catch((err) => next(err));
 };
 
 module.exports = businessesController;
--- server/routes/businesses.js
+++ server/routes/businesses.js
@@ -11,8 +11,31 @@ router.get('/:category', businessesController.getBusinesses, (req, res) => {
    *
    * server response from the url above should be an array of 5 objects.
    * each object contains information on a restaurant
+   *
+   * see yelp api documentation for all valid categories
    */
 
+  const { businesses } = res.locals;
+  res.status(200).json(businesses);
+});
+
+router.get('/:category/:priceLevel', businessesController.getBusinessesByPrice, (req, res) => {
+  /* include lat and lon in query string
+   *
+   * example url where 'restaurants' is the category and '1' is the price level:
+   * http://localhost:5000/businesses/restaurants/1?lat=40.712775&lon=-74.005973
+   *
+   * server response from the url above should be an array of 5 objects.
+   * each object contains information on a restaurant whose price range is that of 1
+   *
+   * valid price levels:
+   * 1 = $
+   * 2 = $$
+   * 3 = $$$
+   * 4 = $$$$
+   *
+   * see yelp api documentation for more details
+   */
   const { businesses } = res.locals;
   res.json(businesses);
 });
